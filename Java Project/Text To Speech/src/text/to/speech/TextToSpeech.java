e922a6ce541092c8cc403c176da01a33f3ebad93
mohammed277 <64231779+mohammed277@users.noreply.github.com>
2021-07-24 21:58:45 +0200

    Add files via upload

---
+++ Java Project/Text To Speech/src/text/to/speech/TextToSpeech.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package text.to.speech;
+
+/**
+ *
+ * @author mohammed
+ */
+public class TextToSpeech {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
